c50d704f87764442fb6075c6948aca63399232dd
CreateTheUniverse <shay.dwight+create@gmail.com>
2016-07-24 20:43:34 -0600

    Add files via upload
    Adding main.js to restore functionality

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,46 @@
-console.log('This would be the main JS file.');
+var protrons = 0;
+var neutrons = 0;
+var electrons = 0;
+
+function protClick(number){
+    protrons = protrons + number;
+    document.getElementById("protrons").innerHTML = protrons;
+};
+
+function neutClick(number){
+    neutrons = neutrons + number;
+    document.getElementById("neutrons").innerHTML = neutrons;
+};
+
+function elecClick(number){
+    electrons = electrons + number;
+    document.getElementById("electrons").innerHTML = electrons;
+};
+
+var Hydrogen = 0;
+
+function buyHydrogen(){
+    if(protrons >= 1 && electrons >= 1){
+        Hydrogen = Hydrogen + 1;
+        protrons = protrons - 1;
+        electrons = electrons - 1;
+        document.getElementById('H').innerHTML = Hydrogen;
+        document.getElementById('protrons').innerHTML = protrons;
+        document.getElementById('electrons').innerHTML = electrons;
+
+    }
+    
+}
+
+var HCreators = 0;
+
+function HCreator(){
+    var HCreatorCost = Math.floor(10 * Math.pow(1.1,HCreators));
+    if(Hydrogen >= HCreatorCost){
+        HCreators= HCreators + 1;
+        Hydrogen = Hydrogen - HCreatorCost;
+        document.getElementById('HCreators').innerHTML = HCreator;
+        document.getElementById('Hydrogen').innerHTML = Hydrogen;
+        
+        }
+}
